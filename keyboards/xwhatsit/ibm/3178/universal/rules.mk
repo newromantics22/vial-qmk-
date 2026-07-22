5c269245e4767e7fe22ccfbb9a3d0cf5e382e375
newromantics22 <69186276+newromantics22@users.noreply.github.com>
2026-07-22 19:13:56 +0900

    Update rules.mk

--- keyboards/xwhatsit/ibm/3178/universal/rules.mk
+++ keyboards/xwhatsit/ibm/3178/universal/rules.mk
@@ -19,6 +19,9 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 RAW_ENABLE = yes
 
+VIAL_ENABLE = no
+VIAL_INSECURE_ENABLE = no
+
 CUSTOM_MATRIX=lite
 SRC += keyboards/xwhatsit/matrix.c keyboards/xwhatsit/util_comm.c
 HAPTIC_ENABLE += SOLENOID
